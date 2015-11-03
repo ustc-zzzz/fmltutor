9b7b8b52eb037364b095d7288d25138a3a009162
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2015-11-03 18:36:53 +0800

    v2.1.2

--- src/main/java/com/github/ustc_zzzz/fmltutor/block/BlockGrassBlock.java
+++ src/main/java/com/github/ustc_zzzz/fmltutor/block/BlockGrassBlock.java
@@ -1,8 +1,15 @@
 package com.github.ustc_zzzz.fmltutor.block;
 
+import com.github.ustc_zzzz.fmltutor.common.EventLoader;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 
 public class BlockGrassBlock extends Block
 {
@@ -13,5 +20,21 @@ public class BlockGrassBlock extends Block
         this.setHardness(0.5F);
         this.setStepSound(soundTypeGrass);
         this.setCreativeTab(CreativeTabs.tabBlock);
+    }
+
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
+            EnumFacing side, float hitX, float hitY, float hitZ)
+    {
+        EventLoader.PlayerRightClickGrassBlockEvent event;
+        event = new EventLoader.PlayerRightClickGrassBlockEvent(playerIn, pos, worldIn);
+        EventLoader.EVENT_BUS.post(event);
+        if (!event.isCanceled() && !worldIn.isRemote)
+        {
+            worldIn.setBlockToAir(pos);
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/com/github/ustc_zzzz/fmltutor/common/EventLoader.java
+++ src/main/java/com/github/ustc_zzzz/fmltutor/common/EventLoader.java
@@ -1,15 +1,25 @@
 package com.github.ustc_zzzz.fmltutor.common;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityTNTPrimed;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
+import net.minecraftforge.fml.common.eventhandler.EventBus;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 public class EventLoader
 {
+    public static final EventBus EVENT_BUS = new EventBus();
+
     public EventLoader()
     {
         MinecraftForge.EVENT_BUS.register(this);
+        EventLoader.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
@@ -29,6 +39,31 @@ public class EventLoader
         {
             String info = String.format("%s interacts with: %s", event.entityPlayer.getName(), event.pos);
             ConfigLoader.logger().info(info);
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerClickGrassBlock(PlayerRightClickGrassBlockEvent event)
+    {
+        if (!event.world.isRemote)
+        {
+            BlockPos pos = event.pos;
+            Entity tnt = new EntityTNTPrimed(event.world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, null);
+            event.world.spawnEntityInWorld(tnt);
+        }
+    }
+
+    @Cancelable
+    public static class PlayerRightClickGrassBlockEvent extends net.minecraftforge.event.entity.player.PlayerEvent
+    {
+        public final BlockPos pos;
+        public final World world;
+
+        public PlayerRightClickGrassBlockEvent(EntityPlayer player, BlockPos pos, World world)
+        {
+            super(player);
+            this.pos = pos;
+            this.world = world;
         }
     }
 }
