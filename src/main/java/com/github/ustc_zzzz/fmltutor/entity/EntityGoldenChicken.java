5879401e0abeb6a1ecb319cc54f17852b319b52b
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2016-05-12 12:06:50 +0800

    v3.1.5

--- src/main/java/com/github/ustc_zzzz/fmltutor/entity/EntityGoldenChicken.java
+++ src/main/java/com/github/ustc_zzzz/fmltutor/entity/EntityGoldenChicken.java
@@ -2,12 +2,18 @@ package com.github.ustc_zzzz.fmltutor.entity;
 
 import com.github.ustc_zzzz.fmltutor.item.ItemLoader;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.attributes.IAttribute;
 import net.minecraft.entity.ai.attributes.RangedAttribute;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
 public class EntityGoldenChicken extends EntityChicken
@@ -19,6 +25,7 @@ public class EntityGoldenChicken extends EntityChicken
     {
         super(worldIn);
         this.setSize(1.2F, 1.8F);
+        this.tasks.addTask(8, new AIStackBlock(this));
     }
 
     @Override
@@ -79,5 +86,47 @@ public class EntityGoldenChicken extends EntityChicken
             this.dropItem(ItemLoader.goldenEgg, 1);
         }
         super.dropFewItems(arg1, arg2);
+    }
+
+    protected static class AIStackBlock extends EntityAIBase
+    {
+        private final EntityGoldenChicken entity;
+
+        public AIStackBlock(EntityGoldenChicken entity)
+        {
+            this.entity = entity;
+        }
+
+        @Override
+        public boolean shouldExecute()
+        {
+            return this.entity.worldObj.getGameRules().getBoolean("mobGriefing");
+        }
+
+        @Override
+        public void updateTask()
+        {
+            BlockPos entityPos = new BlockPos(entity);
+            if (entity.worldObj.isAirBlock(entityPos.up()))
+            {
+                AxisAlignedBB aabb = AxisAlignedBB.fromBounds(entityPos.getX() - 1, entityPos.getY(),
+                        entityPos.getZ() - 1, entityPos.getX() + 2, entityPos.getY() + 1, entityPos.getZ() + 2);
+                for (Object e : entity.worldObj.getEntitiesWithinAABB(EntityItem.class, aabb))
+                {
+                    ItemStack stack = ((EntityItem) e).getEntityItem();
+                    Block block = Block.getBlockFromItem(stack.getItem());
+                    if (block != null)
+                    {
+                        entity.setLocationAndAngles(entity.posX, entity.posY + 1, entity.posZ, entity.rotationYaw,
+                                entity.rotationPitch);
+                        entity.worldObj.setBlockState(entityPos, block.getDefaultState());
+                        if (--stack.stackSize <= 0)
+                        {
+                            ((EntityItem) e).setDead();
+                        }
+                    }
+                }
+            }
+        }
     }
 }
