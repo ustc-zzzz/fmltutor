8c03682e9442c4207ec25e664ea3f1657daaa586
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2016-11-20 13:05:53 +0800

    Maybe there is a conflict

--- book.js
+++
@@ -1,3 +0,0 @@
-if (location.hostname.search('gitbook') >= 0) {
-    location.href = "//fmltutor.ustc-zzzz.net" + location.pathname.substr(17);
-}
---
+++ resources/book.js
@@ -0,0 +1,3 @@
+if (window.location.hostname.search('gitbook') >= 0) {
+    window.location.href = "//fmltutor.ustc-zzzz.net" + window.location.pathname.substr(17);
+}
